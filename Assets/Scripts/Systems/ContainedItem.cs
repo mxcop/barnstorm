3a2abc6823355623ff1cb7e3f2a0d451a448e11b
MaxCoppen <44031065+mxcop@users.noreply.github.com>
2022-03-17 16:48:42 +0100

    Inventory and ContainedItem

---
+++ Assets/Scripts/Systems/ContainedItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Systems.Inventory
+{
+    /// <summary>
+    /// A container for a type of item.
+    /// Which keeps track of the number of the items.
+    /// </summary>
+    public class ContainedItem<T> where T : Item
+    {
+        /// <summary>
+        /// The number of items contained.
+        /// </summary>
+        public int num;
+
+        /// <summary>
+        /// The type of item contained.
+        /// </summary>
+        public T item;
+
+        public ContainedItem(T item, int num)
+        {
+            this.item = item;
+            this.num = num;
+        }
+    }
+}
