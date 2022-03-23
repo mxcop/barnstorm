d11c50eb0285e239fdec70220e27cb3d9bfba0a3
FunkyBuritto <milanb043@gmail.com>
2022-03-23 11:39:01 +0100

    Sprites and fonts

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +9,19 @@ public class TurretController : MonoBehaviour
     public Food ammunition;
 
     [SerializeField] private Sprite[] sprite;
-    [SerializeField] private float reloadTime;
+    [SerializeField] private Transform[] shootPoints;
     [SerializeField] private GameObject turret;
     [SerializeField] private GameObject targetEmemy;
-
+    [SerializeField] private float reloadTime;
+    
     private SpriteRenderer turretsr;
     private int rotationState = 0;
     private bool shooting = false;
-    private float shootAngle;
+    [SerializeField] private float shootAngle;
 
     void Start()
     {
-        turretsr = turret.GetComponent<SpriteRenderer>();   
+        turretsr = turret.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -27,11 +29,14 @@ public class TurretController : MonoBehaviour
         // Get the Position of the mouse in world coordinates
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
-        shootAngle = AngleBetweenPoints(turret.transform.position, mousePos);
-
+        float spriteAngle = AngleBetweenPoints(turret.transform.position, targetEmemy.transform.position);
+      
         // Mapping the angle to a value between 0-8 so we can get the sprite direction that faces the correct position
-        int round = Mathf.RoundToInt(shootAngle / 360 * 16);
-        turretsr.sprite = sprite[round == 16 ? 0 : round];
+        int round = Mathf.RoundToInt(spriteAngle / 360 * 16);
+        rotationState = round == 16 ? 0 : round;
+        turretsr.sprite = sprite[rotationState];
+
+        shootAngle = AngleBetweenPoints(shootPoints[rotationState].position, targetEmemy.transform.position);
 
         if (!shooting && targetEmemy != null)
             StartCoroutine(Shoot());
@@ -40,9 +45,8 @@ public class TurretController : MonoBehaviour
     IEnumerator Shoot()
     {
         shooting = true;
-        GameObject proj = Instantiate(ammunition.projectile, turret.transform.position, Quaternion.identity);
+        GameObject proj = Instantiate(ammunition.projectile, shootPoints[rotationState].position, Quaternion.identity);
         proj.transform.localRotation = Quaternion.Euler(0,0, shootAngle - 90);
-        Debug.Log(proj.transform.eulerAngles);
         yield return new WaitForSeconds(reloadTime);
         shooting = false;
     }
