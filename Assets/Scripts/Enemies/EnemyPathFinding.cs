744df053da41cfb61683f7466c5cd707e9c1581a
FunkyBuritto <milanb043@gmail.com>
2022-04-11 18:33:29 +0200

    Astar Pathfinding Optimizations

--- Assets/Scripts/Enemies/EnemyPathFinding.cs
+++ Assets/Scripts/Enemies/EnemyPathFinding.cs
@@ -14,26 +14,32 @@ public class EnemyPathFinding : MonoBehaviour
 
     [Header("Collision Map")]
     [SerializeField] [Tooltip("The collison layer that the enemies will avoid")] private LayerMask collisionLayer;
-    [SerializeField] [Tooltip("How many times a single tile will be devided into small chunks")] private float resolution;
+    [SerializeField] private int stepSize;
     [SerializeField] [Tooltip("The radius the algorithm will work with from 0,0")] private int collisionRange;
 
     private int[][] collisionMap;
 
+    /// <summary>
+    /// This method creates and updates the collision map in the form of a 2d int array
+    /// </summary>
     void PopulateCollisionMap() {
+        int rangeScale = collisionRange * 2 * stepSize;
         // Initialize the First dimension of the array and loop over it
-        collisionMap = new int[collisionRange * 2][];
-        for (int i = 0; i < collisionRange * 2; i++){
+        collisionMap = new int[rangeScale][];
+        for (int i = 0; i < rangeScale; i++){
 
             // Initialize Second dimension of the array and loop over it
-            collisionMap[i] = new int[collisionRange * 2];
-            for (int j = 0; j < collisionRange * 2; j++){
-                
+            collisionMap[i] = new int[rangeScale];
+            for (int j = 0; j < rangeScale; j++){
                 // If we detect a collision the collisionMap[i][j] = 1 otherwise its 0
-                collisionMap[i][j] = Physics2D.OverlapCircle(new Vector2(j - collisionRange, i - collisionRange), 0.45f, collisionLayer) ? 1 : 0;
+                collisionMap[i][j] = Physics2D.OverlapCircle(new Vector2(j / (float)stepSize - collisionRange, i / (float)stepSize - collisionRange), 0.49f / stepSize, collisionLayer) ? 1 : 0;
             }
         }
     }
 
+    /// <summary>
+    /// A temporary method to display the obstacles and walkeble areas in the scene
+    /// </summary>
     void DisplayMap() {
         // Loop over 2d int array
         for (int i = 0; i < collisionMap.Length; i++) {
@@ -41,26 +47,32 @@ public class EnemyPathFinding : MonoBehaviour
 
                 // If it is a obstacle spawn the inactive object else spawn the active object
                 if (collisionMap[i][j] == 1) {
-                    Instantiate(obstacleObj, new Vector2(j - collisionRange, i - collisionRange), Quaternion.identity);
+                    Instantiate(obstacleObj, new Vector2((float)(j / (float)stepSize - collisionRange), (float)(i / (float)stepSize - collisionRange)), Quaternion.identity);
                 } 
                 else {
-                    Instantiate(activeObj, new Vector2(j - collisionRange, i - collisionRange), Quaternion.identity);
+                    Instantiate(activeObj, new Vector2((float)(j / (float)stepSize - collisionRange), (float)(i / (float)stepSize - collisionRange)), Quaternion.identity);
                 }     
             }
         }
     }
 
+    /// <summary>
+    /// A temporary method to display the A* path in the scene
+    /// </summary>
     void DisplayResults(List<Vector2Int> result) {
         for (int i = 0; i < result.Count; i++) {
-            Instantiate(resultObj, new Vector2(result[i].x - collisionRange, result[i].y - collisionRange), Quaternion.identity);
+            //Instantiate(resultObj, new Vector2((float)(result[i].x - collisionRange) / stepSize, (float)(result[i].y - collisionRange) / stepSize), Quaternion.identity);
+            Instantiate(resultObj, new Vector2((float)(result[i].x / (float)stepSize - collisionRange) , (float)(result[i].y / (float)stepSize - collisionRange)), Quaternion.identity);
         }
     }
 
-    // Start is called before the first frame update
     void Start() {
         StartCoroutine(UpdateLoop());
     }
 
+    /// <summary>
+    /// A temporary interface to update the map and path
+    /// </summary>
     IEnumerator UpdateLoop() {
         yield return new WaitForSeconds(1f);
         GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
@@ -69,13 +81,7 @@ public class EnemyPathFinding : MonoBehaviour
         }
         PopulateCollisionMap();
         DisplayMap();
-        DisplayResults(new Astar(Astar.ConvertToBoolArray(collisionMap), new Vector2Int((int)(start.position.x + collisionRange), (int)(start.position.y + collisionRange)), new Vector2Int((int)(end.position.x + collisionRange), (int)(end.position.y + collisionRange)), Astar.Type.DiagonalFree).Result);
+        DisplayResults(new Astar(Astar.ConvertToBoolArray(collisionMap), new Vector2Int((int)(start.position.x * stepSize + collisionRange * stepSize), (int)(start.position.y * stepSize + collisionRange * stepSize)), new Vector2Int((int)(end.position.x * stepSize + collisionRange * stepSize), (int)(end.position.y * stepSize + collisionRange * stepSize)), Astar.Type.DiagonalFree).Result);
         StartCoroutine(UpdateLoop());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
