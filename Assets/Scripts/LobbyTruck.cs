a9c0c39cb6bcd3f8fd657d82fac900fe8d339bb0
FunkyBuritto <milanb043@gmail.com>
2022-04-18 12:20:56 +0200

    Sorting layer fix and lobby countdown

--- Assets/Scripts/LobbyTruck.cs
+++ Assets/Scripts/LobbyTruck.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class LobbyTruck : MonoBehaviour
 {
+    [SerializeField] private int startTimer;
+    [SerializeField] private Text timerText;
+    private bool inCountdown;
+
     [SerializeField] private GameObject cover;
     private bool coverOn = true;
     private List<GameObject> players = new List<GameObject>();
     public UnityEvent OnAllPlayersReady;
 
+    private void Start()
+    {
+        timerText.text = "";
+    }
 
     private void FixedUpdate()
     {
@@ -29,9 +38,10 @@ public class LobbyTruck : MonoBehaviour
 
     private void Update()
     {
-        if(players.Count > 0)
-        {            
-
+        if(players.Count > 0 && !inCountdown)
+        {
+            StartCoroutine(StartCountdown());
+            /*
             if(players.Count >= PersistentPlayerManager.main.PlayerCount)
             {
                 // start fast countdown
@@ -42,8 +52,27 @@ public class LobbyTruck : MonoBehaviour
             else
             {
                 // start slow countdown
+            }*/
+        }
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        inCountdown = true;
+        for (int i = 0; i < startTimer; i++) {
+            if(players.Count >= PersistentPlayerManager.main.PlayerCount) {
+                timerText.text = "Game starts in " + (startTimer - i);
+                yield return new WaitForSeconds(1);
+            }
+            else {
+                timerText.text = "";
+                inCountdown = false;
+                yield break;
             }
         }
+        OnAllPlayersReady?.Invoke();
+        timerText.text = "";
+        inCountdown = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
